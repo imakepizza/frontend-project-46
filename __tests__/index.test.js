5c3d8435abdf0801b60214f45bc1e441ba0cf4a0
imakepizza <anpash1317@gmail.com>
2023-02-04 16:52:42 +0900

    fix cc and linter

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,15 +1,14 @@
-import genDiff from '../src/index.js'
-import path from 'path'
-import fs from 'fs'
+import genDiff from '../src/index.js';
+import path from 'path';
+import fs from 'fs';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-
-const jsonPath1 = readFile('file1.json')
-const jsonPath2 = readFile('file2.json')
+const jsonPath1 = readFile('file1.json');
+const jsonPath2 = readFile('file2.json');
 const expected = `{
 - follow: false
   host: hexlet.io
@@ -18,10 +17,10 @@ const expected = `{
 + timeout: 20
 + verbose: true
 
-}`
+}`;
 
 describe('genDiff', () => {
-	it('should work', ()=> {
-		expect(genDiff(jsonPath1, jsonPath2) === expected).toBe(true)
-	})
+  it('should work', () => {
+    expect(genDiff(jsonPath1, jsonPath2) === expected).toBe(true);
+  });
 });
--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
-import fs from 'fs'
-import _ from 'lodash'
+import fs from 'fs';
+import _ from 'lodash';
+
 const genDiff = (filepath1, filepath2) => {
-	// const f1 = fs.readFileSync(file1, 'utf-8')
-	// const f2 = fs.readFileSync(file2, 'utf-8')
-	const obj1 = JSON.parse(filepath1);
-	const obj2 = JSON.parse(filepath2);
-	const keys1 = _.keys(obj1);
-	const keys2 = _.keys(obj2);
-	const uniqKeys = _.union(keys1, keys2);
-	const sortedKeys = _.sortBy(uniqKeys)
-	const result = sortedKeys.reduce((acc, key) => {
-		if(!_.has(obj1, key)) {
-			acc.push(`+ ${key}: ${obj2[key]}\n`)
-		}else if(!_.has(obj2, key)) {
-			acc.push(`- ${key}: ${obj1[key]}\n`)
-		}else if(obj1[key] === obj2[key]) {
-			acc.push(`  ${key}: ${obj1[key]}\n`)
-		}else if(obj1[key] !== obj2[key]) {
-			acc.push(`- ${key}: ${obj1[key]}\n`)
-			acc.push(`+ ${key}: ${obj2[key]}\n`)
-		}
-		return acc;
-	}, [])
-	return `{\n${result.join('')}\n}`;
+  // const f1 = fs.readFileSync(file1, 'utf-8')
+  // const f2 = fs.readFileSync(file2, 'utf-8')
+  const obj1 = JSON.parse(filepath1);
+  const obj2 = JSON.parse(filepath2);
+  const keys1 = _.keys(obj1);
+  const keys2 = _.keys(obj2);
+  const uniqKeys = _.union(keys1, keys2);
+  const sortedKeys = _.sortBy(uniqKeys);
+  const result = sortedKeys.reduce((acc, key) => {
+    if (!_.has(obj1, key)) {
+      acc.push(`+ ${key}: ${obj2[key]}\n`);
+    } else if (!_.has(obj2, key)) {
+      acc.push(`- ${key}: ${obj1[key]}\n`);
+    } else if (obj1[key] === obj2[key]) {
+      acc.push(`  ${key}: ${obj1[key]}\n`);
+    } else if (obj1[key] !== obj2[key]) {
+      acc.push(`- ${key}: ${obj1[key]}\n`);
+      acc.push(`+ ${key}: ${obj2[key]}\n`);
+    }
+    return acc;
+  }, []);
+  return `{\n${result.join('')}\n}`;
+};
 
-}
-
-export default genDiff
-
+export default genDiff;
